b158557147b474ce1d22a2f9d6d3dc54e38604fd
ZzLuffyzZ <toan1998vn1@gmail.com>
2021-05-13 07:58:57 +0700

    Update userController.js

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const multer = require('multer');
 const mongoose = require('mongoose')
-const User = require('../model/user')
+const User = require('../model/User')
 const bcrypt = require('bcrypt')
 
 const storage = multer.diskStorage({
