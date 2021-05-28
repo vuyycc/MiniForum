e7e0aef812aed7e1babb7b932b2139005143d004
ZzLuffyzZ <toan1998vn1@gmail.com>
2021-05-28 13:58:10 +0700

    Create constants.js

---
+++ server/constants.js
@@ -0,0 +1,19 @@
+const multer = require('multer')
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().getTime() + file.originalname)
+    }
+})
+
+
+
+const upload = multer({
+    storage: storage,
+    
+})
+module.exports={upload}
