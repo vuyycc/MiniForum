3a856b91ffa4f58f5298b2384ee3ccf68435153f
abcdefg134 <toan198vn1@gmail.com>
2021-05-30 09:22:35 +0700

    Create authController.js

---
+++ server/controller/authController.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const User = require('../model/User')
+const bcrypt = require('bcrypt')
+router.post('/login', async (req,res)=>{
+    const {email, password} = req.body;
+    const user = await User.findOne({
+        email: email
+    })
+    let checkPass = false
+    if(user){
+        checkPass = bcrypt.compareSync(password, user.password)
+    }
+    if(user, checkPass){
+        const accessToken = jwt.sign({email: user.email, _id: user._id}, process.env.SECRET_KEY);
+        return res.json({
+            accessToken
+        })
+    }else {
+        return res.status(400).send({err:'Email or password incorrect'})
+    }
+})
+
+module.exports = router
