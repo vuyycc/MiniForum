a9fcb43642c852ce353e4cde379c78992c5ccf1a
abcdefg134 <toan198vn1@gmail.com>
2021-05-30 09:27:05 +0700

    Create authenMiddleware.js

---
+++ server/helper/authenMiddleware.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const User = require('../model/User')
+
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, process.env.SECRET_KEY,async (err, payload) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+            console.log(payload)
+          let user = await User.findById(payload._id).exec()
+       
+            req.authenticateUser = user
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = { authenticateJWT }
