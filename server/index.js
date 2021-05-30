8fe8b8bc43d0a2fb10994d3e0369fd9b47b274c7
abcdefg134 <toan198vn1@gmail.com>
2021-05-30 09:27:09 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = 8797;
 const cors = require('cors')
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
-
+const middleware = require('./helper/authenMiddleware')
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -13,6 +13,7 @@ app.use(express.static('uploads'))
 
 const UserRouter = require('./controller/userController')
 const PostRouter = require('./controller/postController')
+const authRouter = require('./controller/authController')
 const CommentRounter = require('./controller/commentController')
 var mongoDB = 'mongodb://localhost:27017/projectForum';
 mongoose.connect(mongoDB, function (err) {
@@ -23,10 +24,10 @@ mongoose.connect(mongoDB, function (err) {
 mongoose.Promise = global.Promise;
 //Lấy kết nối mặc định
 var db = mongoose.connection;
-
-app.use('/user', UserRouter)
-app.use('/post', PostRouter)
-app.use('/comment', CommentRounter)
+app.use('/', authRouter)
+app.use('/user',middleware.authenticateJWT ,UserRouter)
+app.use('/post',middleware.authenticateJWT , PostRouter)
+app.use('/comment',middleware.authenticateJWT , CommentRounter)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(PORT, () => { console.log("Server started on http://localhost:" + PORT) })
