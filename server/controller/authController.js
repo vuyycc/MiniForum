121e311d0cf595f84494ca20e56affc0b85ba0b6
Abcdefg134 <84908893+Abcdefg134@users.noreply.github.com>
2021-06-03 19:37:01 +0700

    update render role

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -13,7 +13,7 @@ router.post('/login', async (req,res)=>{
         checkPass = bcrypt.compareSync(password, user.password)
     }
     if(user, checkPass){
-        const accessToken = jwt.sign({email: user.email, _id: user._id}, process.env.SECRET_KEY);
+        const accessToken = jwt.sign({email: user.email, _id: user._id, role: user.role}, process.env.SECRET_KEY);
         return res.json({
             accessToken
         })
